1facae95227507e9bcc29fa304ecbab6241de2c5
Ariana Faraday <ariana@thinkful.com>
2018-08-28 19:16:55 -0400

    adds remaining solution code

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -24,6 +24,9 @@ BlogPosts.create("Lions and tigers and bears oh my", lorem(), "Lefty Lil");
 // and return JSON objects of stored blog posts.
 // send back JSON representation of all blog posts
 // on GET requests to root
+router.get("/", (req, res) => {
+  res.json(BlogPosts.get());
+});
 
 // add endpoint for POST requests, which should cause a new
 // blog post to be added (using `BlogPosts.create()`). It should
@@ -32,6 +35,25 @@ BlogPosts.create("Lions and tigers and bears oh my", lorem(), "Lefty Lil");
 // send a 400 error if the post doesn't contain
 // `title`, `content`, and `author`
 
+router.post("/", (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ["title", "content", "author"];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = BlogPosts.create(
+    req.body.title,
+    req.body.content,
+    req.body.author
+  );
+  res.status(201).json(item);
+});
+
 // add endpoint for PUT requests to update blogposts. it should
 // call `BlogPosts.update()` and return the updated post.
 // it should also ensure that the id in the object representing
@@ -39,6 +61,42 @@ BlogPosts.create("Lions and tigers and bears oh my", lorem(), "Lefty Lil");
 // following required fields are in request body: `id`, `title`,
 // `content`, `author`, `publishDate`
 
+router.put("/:id", (req, res) => {
+  const requiredFields = ["id", "title", "content", "author", "publishDate"];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${
+      req.params.id
+    }) and request body id ``(${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post with id \`${req.params.id}\``);
+  BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
+  });
+  res.status(204).end();
+});
+
 // add endpoint for DELETE requests. These requests should
 // have an id as a URL path variable and call
 // `BlogPosts.delete()`
+
+router.delete("/:id", (req, res) => {
+  BlogPosts.delete(req.params.id);
+  console.log(`Deleted blog post with id \`${req.params.ID}\``);
+  res.status(204).end();
+});
+
+module.exports = router;
