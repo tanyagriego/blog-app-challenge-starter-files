54645d8468f5a291ab2c59d0b1ca2c8be1141b6e
tanyagriego <tanya.griego@gmail.com>
2018-12-28 16:58:42 -0800

    wrote get requests

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+const {DATABASE_URL, PORT } = require ('./config');
+const { BlogPost } = require('./models');
 
 const bprouter = require("./blogPostsRouter");
 
@@ -8,6 +13,27 @@ const app = express();
 app.use(morgan("common"));
 app.use(express.json());
 
+app.get('/posts', (req, res) => {
+  BlogPost
+  .find()
+  .then(posts => {
+    res.json(posts.map(post => post.serialize()));
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({ error: 'something went wrong.'});
+  });
+});
+
+app.get('/posts/:id', (req, res) => {
+  BlogPost
+  .findById(req.params.id)
+  .then(post => res.json(post.serialize()))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({error: 'something went wrong wrong wrong'});
+  });
+});
 // you need to import `blogPostsRouter` router and route
 
 // requests to HTTP requests to `/blog-posts` to `blogPostsRouter`
